b182fd8899c39c0ec3b710ea1594a629d019b50e
MedAli BOUDLAL <boudlalmohamedali@gmail.com>
2015-09-16 14:47:15 +0200

    moi

---
+++ var/config/index/config.ini.php
@@ -0,0 +1,9 @@
+;<?php die(''); ?>
+;for security reasons , don't remove or modify the first line
+
+startModule=neoinformatique
+startAction="default:index"
+
+[responses]
+[modules]
+neoinformatique.access=2
---
+++ var/config/installer.ini.php
@@ -0,0 +1,34 @@
+;<?php die(''); ?>
+; for security reasons , don't remove or modify the first line
+; don't modify this file if you don't know what you do. it is generated automatically by jInstaller
+
+
+[index]
+jelix.installed=1
+jelix.version=1.5.5
+jacl.installed=0
+jacl.version=
+jacl2.installed=0
+jacl2.version=
+jacl2db.installed=0
+jacl2db.version=
+jacldb.installed=0
+jacldb.version=
+jauth.installed=0
+jauth.version=
+jauthdb.installed=0
+jauthdb.version=
+jpref.installed=0
+jpref.version=
+jsoap.installed=0
+jsoap.version=
+junittests.installed=0
+junittests.version=
+jelix.version.date="2015-02-24 16:02"
+jelix.firstversion=1.5.5
+jelix.firstversion.date="2015-02-24 16:02"
+neoinformatique.installed=1
+neoinformatique.version=0.1pre
+[__modules_data]
+jelix.contexts="db:default"
+
